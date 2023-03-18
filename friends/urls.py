0e351648f05a69f3836e828c71990cfcf83f2574
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2023-03-18 01:30:29 +0100

    last change to friends app

--- friends/urls.py
+++ friends/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'friends'
 urlpatterns = [
   path("", views.index, name="index"),
-  path("<int:friend_id>/show", views.show, name="show")
+  path("create", views.create, name="create"),
+  path("save", views.save, name="save"),
+  path("<int:friend_id>/show", views.show, name="show"),
+  path("<int:friend_id>/delete", views.delete, name="delete"),
+  path("<int:friend_id>/edit", views.edit, name="edit"),
+  path("<int:friend_id>/update", views.update, name="update")
 ]
