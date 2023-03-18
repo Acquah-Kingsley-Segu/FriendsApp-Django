108fde87f6f2dbd89438ea70781b168e409d5bc3
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2023-03-18 01:24:01 +0100

    Include path  registration app and auth app

--- friends_app/urls.py
+++ friends_app/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('friends/', include('friends.urls')),
     path('admin/', admin.site.urls),
+    path('friends/', include('friends.urls')),
+    path('registration/', include('registration.urls')),
+    path('registration/', include('django.contrib.auth.urls')),
 ]
